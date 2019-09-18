e01bc9c476045087af883a5beb6e2044b2096c95
Arrackisarookie <nicearrack@163.com>
2019-09-18 20:48:10 +0800

    Add nullable setting.

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,9 @@ from blog.extensions import db
 class Admin(db.Model):
     __tablename__ = 'admin'
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(32))
-    password_hash = db.Column(db.String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     blog_title = db.Column(db.String(64))
     blog_sub_title = db.Column(db.String(128))
     about = db.Column(db.Text)
@@ -26,19 +26,19 @@ class Admin(db.Model):
 class Category(db.Model):
     __tablename__ = 'category'
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
     articles = db.relationship('Article', backref='category')
 
 
 class Article(db.Model):
     __tablename__ = 'article'
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(128))
-    body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    can_comment = db.Column(db.Boolean, default=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    can_comment = db.Column(db.Boolean, default=True, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     comments = db.relationship('Comment', backref='article', cascade='all, delete-orphan')
 
@@ -46,11 +46,11 @@ class Article(db.Model):
 class Comment(db.Model):
     __tablename__ = 'comment'
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    author = db.Column(db.String(32))
-    body = db.Column(db.Text)
-    reviewed = db.Column(db.Boolean, default=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(32), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    reviewed = db.Column(db.Boolean, default=False, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
     article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
     replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
