ca65678445c8552e35245691e2f498b3c0bb7bc1
Arrackisarookie <nicearrack@163.com>
2019-10-11 18:30:39 +0800

    Add error handler.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 from blog.admin import admin
 from blog.extensions import (
@@ -15,7 +15,7 @@ def create_app(config_name):
 
     register_blueprint(app)
     register_extensions(app)
-    # register_errors(app)
+    register_errors(app)
 
     admin.init_app(app)
 
@@ -34,17 +34,27 @@ def register_blueprint(app):
     app.register_blueprint(blog.bp)
     from .views import auth
     app.register_blueprint(auth.bp, url_prefix='/auth')
+    from .api import api as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
 
 def register_errors(app):
-    @app.errorhandler(400)
-    def bad_request(e):
-        return render_template('errors/400.html')
+    # @app.errorhandler(400)
+    # def bad_request(e):
+    #     return render_template('errors/400.html')
 
     @app.errorhandler(404)
     def page_not_found(e):
+        if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+            response = jsonify({'error': 'not found'})
+            response.status_code = 404
+            return response
         return render_template('errors/404.html')
 
     @app.errorhandler(500)
     def internal_server_error(e):
+        if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+            response = jsonify({'error': 'internal server error'})
+            response.status_code = 500
+            return response
         return render_template('errors/500.html')
